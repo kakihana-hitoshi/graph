81ccbf39606091bc9739225c59a10f9b324f0d63
kakihana-hitoshi <kakihana.hitoshi.biz@gmail.com>
2022-01-22 10:54:02 +0900

    導入 https://lighthouse-php.com/5/getting-started/installation.html
    composerインストール
    composer require nuwave/lighthouse
    
    実行
    php artisan vendor:publish --tag=lighthouse-schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->register(\Nuwave\Lighthouse\LighthouseServiceProvider::class);
     }
 
     /**
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+
     }
 }
